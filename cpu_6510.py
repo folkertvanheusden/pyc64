6dfc5091c37836c91f8ce3ce4be436822a479a79
Folkert van Heusden <folkert@vanheusden.com>
2020-09-08 16:13:20 +0200

    NOP instruction

---
+++ cpu_6510.py
@@ -0,0 +1,22 @@
+from bus import bus
+
+class cpu_6510:
+    def __init__(self, bus):
+        self.bus = bus
+
+        self.opcodes = [ None ] * 256
+        self.opcodes[0xea] = i_nop
+
+    def reset():
+        self.pc = 0
+
+    def tick():
+        opcode = self.bus.read(self.pc)
+
+        if self.opcodes[opcode]:
+            self.opcodes[opcode](opcode)
+
+        self.pc += 1
+
+    def i_nop(opcode):
+        pass
