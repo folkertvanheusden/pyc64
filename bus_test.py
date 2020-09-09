8e60848459ba840996b8bbb317b23b9f9243fd62
Folkert van Heusden <folkert@vanheusden.com>
2020-09-09 13:55:34 +0200

    65xx emulation tester

---
+++ bus_test.py
@@ -0,0 +1,19 @@
+# (C) 2020 by Folkert van Heusden <mail@vanheusden.com>
+# released under AGPL v3.0
+
+from bus_base import bus_base
+
+class bus_test(bus_base):
+    def __init__(self):
+        fh = open('6502_functional_test.bin', 'rb')
+        self.ram: List[int] = [ int(b) for b in fh.read() ]
+        fh.close()
+
+    def reset(self):
+        pass
+
+    def read(self, addr):
+        return self.ram[addr]
+
+    def write(self, addr, value):
+        self.ram[addr] = value
--- c64.py
+++ c64.py
@@ -1,15 +1,18 @@
 #! /usr/bin/python3
 
-from bus_c64 import bus_c64
+#from bus_c64 import bus_c64
+from bus_test import bus_test
 from cpu_6510 import cpu_6510
 
 class cbm64:
     def __init__(self):
-        self.bus = bus_c64()
+        #self.bus = bus_c64()
+        self.bus = bus_test()
         self.cpu = cpu_6510(self.bus)
 
     def reset(self):
         self.cpu.reset()
+        self.cpu.pc = 0x0400 # for 6502_functional_test.bin
 
     def run(self):
         # for i in range(0, 1024):
@@ -17,8 +20,8 @@ class cbm64:
             # print('%04x' % self.cpu.pc)
             self.cpu.tick()
 
-            if self.cpu.pc == 0xe5cd:
-                break
+#            if self.cpu.pc == 0xe5cd:
+#                break
 
 c64 = cbm64()
 c64.reset()
