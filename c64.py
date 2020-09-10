649af331e54dd3ce4b10f666dbeea4435cdb6721
Folkert van Heusden <folkert@vanheusden.com>
2020-09-10 10:03:33 +0200

    IRQ every 20k cycles

--- c64.py
+++ c64.py
@@ -1,27 +1,25 @@
 #! /usr/bin/python3
 
-#from bus_c64 import bus_c64
-from bus_test import bus_test
+from bus_c64 import bus_c64
 from cpu_6510 import cpu_6510
 
 class cbm64:
     def __init__(self):
-        #self.bus = bus_c64()
-        self.bus = bus_test()
+        self.bus = bus_c64()
         self.cpu = cpu_6510(self.bus)
 
     def reset(self):
         self.cpu.reset()
-        self.cpu.pc = 0x0400 # for 6502_functional_test.bin
 
     def run(self):
-        # for i in range(0, 1024):
+        p_irq_cycles = 0
+
         while True:
-            # print('%04x' % self.cpu.pc)
             self.cpu.tick()
 
-#            if self.cpu.pc == 0xe5cd:
-#                break
+            if self.cpu.cycles - p_irq_cycles >= 20000:
+                p_irq_cycles = self.cpu.cycles
+                self.cpu.IRQ()
 
 c64 = cbm64()
 c64.reset()
