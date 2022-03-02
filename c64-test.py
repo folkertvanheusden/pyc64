1001d4041b865a54999332ff8c969066ef744e53
Folkert van Heusden <folkert@vanheusden.com>
2022-03-02 17:50:30 +0100

    test wrapper

---
+++ c64-test.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python3
+
+# (C) 2020 by Folkert van Heusden <mail@vanheusden.com>
+# License: Apache License v2.0
+
+from bus_test import bus_test
+from cpu_6510 import cpu_6510
+
+class cbm64:
+    def __init__(self):
+        self.bus = bus_test()
+        self.cpu = cpu_6510(self.bus)
+
+    def reset(self):
+        self.cpu.reset()
+        self.cpu.pc = 0x0400
+
+    def run(self):
+        while True:
+            self.cpu.tick()
+
+c64 = cbm64()
+c64.reset()
+c64.run()
