5eca6ae6a991af28be5383bba3b12788c46abf63
folkert van heusden <mail@vanheusden.com>
2022-03-03 13:17:13 +0100

    d012: return 0x00

--- i_o.py
+++ i_o.py
@@ -14,9 +14,11 @@ class i_o(bus_device):
     def read(self, addr):
         # print('I/O read from %04x' % addr)
         if addr == 0xd012:
-            return 0xff
+            return 0x00
+
         return self.data[addr]  # FIXME
 
     def write(self, addr, value):
         # print('I/O write to %04x: %02x' % (addr, value))
+
         self.data[addr] = value  # FIXME
